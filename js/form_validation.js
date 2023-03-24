a15a58a44e1662c83d3c3f403d3d5b4695da7832
cariasj03 <128652671+cariasj03@users.noreply.github.com>
2023-03-24 15:55:37 -0600

    Validation functionality added

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,58 +1,112 @@
+//Variables
 const form = document.getElementById("form");
-const formInputs = document.querySelectorAll("#form input, #form textarea");
-
+const formInputs = document.querySelectorAll(
+  "#form input, #form textarea, #form select"
+);
 const expressions = {
   empty: /^.+$/, //Any character
   letterNumbers: /^[a-zA-ZÀ-ÿ0-9\s]{1,40}$/, // Letters, numbers and spaces, accepts accents
   letters: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letters and spaces, accepts accents
 };
 
+const assetRegistrationFields = {
+  assetName: false,
+  assetDescription: false,
+  assetUnit: false,
+  assetLocation: false,
+  assetStatus: false,
+};
+
+//Functions
+//Validation fuction for the entire form
 const validateForm = function (event) {
   const elementId = event.target.id;
   const elementValue = event.target.value;
   const errorActiveClass = "form-input-error-active";
-  console.log(elementId);
 
-  if (event.target.id == elementId) {
-    //Validates if the input is empty
-    if (!expressions.empty.test(elementValue)) {
-      document.getElementById(`${elementId}-error`).innerHTML =
-        "Este es un campo obligatorio.";
-      document
-        .getElementById(`${elementId}-error`)
-        .classList.add(errorActiveClass);
-    } else {
-      switch (elementId) {
-        //Validates the name input specifically
-        case "asset-name":
-          if (!expressions.letterNumbers.test(elementValue)) {
-            document.getElementById(`${elementId}-error`).innerHTML =
-              "Este campo solo puede contener letras, números y espacios.";
-            document
-              .getElementById(`${elementId}-error`)
-              .classList.add(errorActiveClass);
-          } else {
-            document
-              .getElementById(`${elementId}-error`)
-              .classList.remove("form-input-error-active");
-          }
-          break;
-        //Default option
-        default:
-          document
-            .getElementById(`${elementId}-error`)
-            .classList.remove(errorActiveClass);
-          break;
-      }
-    }
+  switch (elementId) {
+    case "asset-name":
+      validateField(elementId, elementValue, errorActiveClass, "assetName");
+      break;
+    case "asset-description":
+      validateField(
+        elementId,
+        elementValue,
+        errorActiveClass,
+        "assetDescription"
+      );
+      break;
+    case "asset-unit":
+      validateField(elementId, elementValue, errorActiveClass, "assetUnit");
+      break;
+    case "asset-location":
+      validateField(elementId, elementValue, errorActiveClass, "assetLocation");
+      break;
+    case "asset-status":
+      validateField(elementId, elementValue, errorActiveClass, "assetStatus");
+      break;
   }
 };
 
+//Validation fuction for all fields
+const validateField = function (
+  elementId,
+  elementValue,
+  errorActiveClass,
+  field
+) {
+  console.log(assetRegistrationFields);
+
+  //Validates if the input field is empty
+  if (!expressions.empty.test(elementValue)) {
+    document.getElementById(`${elementId}-error`).innerHTML =
+      "Este es un campo obligatorio.";
+    document
+      .getElementById(`${elementId}-error`)
+      .classList.add(errorActiveClass);
+    assetRegistrationFields[field] = false;
+  } else {
+    document
+      .getElementById(`${elementId}-error`)
+      .classList.remove(errorActiveClass);
+    assetRegistrationFields[field] = true;
+  }
+};
+
+//Asset registration button
+const assetRegistrationBtn = function () {
+  if (
+    assetRegistrationFields.assetName &&
+    assetRegistrationFields.assetDescription &&
+    assetRegistrationFields.assetUnit &&
+    assetRegistrationFields.assetLocation &&
+    assetRegistrationFields.assetStatus
+  ) {
+    successAlert("Registro exitoso", "El activo ha sido registrado con éxito.");
+    form.reset();
+    assetRegistrationFields.assetName = false;
+    assetRegistrationFields.assetDescription = false;
+    assetRegistrationFields.assetUnit = false;
+    assetRegistrationFields.assetLocation = false;
+    assetRegistrationFields.assetStatus = false;
+  } else {
+    errorAlert("Hay campos obligatorios sin llenar.");
+  }
+};
+
+//Event Listeners
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  assetRegistrationBtn();
 });
 
-formInputs.forEach(function (input) {
-  input.addEventListener("blur", validateForm);
-  input.addEventListener("keyup", validateForm);
-});
+const validation = function () {
+  formInputs.forEach(function (input) {
+    input.addEventListener("blur", validateForm);
+    input.addEventListener("keyup", validateForm);
+    input.addEventListener("click", validateForm);
+  });
+};
+
+//Function calls
+validation();
