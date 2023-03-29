4566fe266d7fc4ec7fab38527b6d4670e70d181c
AnaCRgogni <106693889+AnaCRgogni@users.noreply.github.com>
2023-03-28 23:54:34 -0600

    Added requests_asset_transfer + javascript
    Added content to the CSS and JS to help make the site look like the wireframe. It still needs its alignment to be improved.

---
+++ js/transfer_forms_provisional_content.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+
+    let assetId = document.getElementById("assetId");
+    assetId.defaultValue = "000001";
+
+    let assetUnit = document.getElementById("assetUnit");
+    assetUnit.defaultValue = "Heredia";
+
+    let assetName = document.getElementById("assetName");
+    assetName.defaultValue = "Impresora EPSON F570";
+
+    assetId.disabled = true;
+
+    assetUnit.disabled = true;
+
+    assetName.disabled = true;
+
+}
