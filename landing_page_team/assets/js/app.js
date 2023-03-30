566aa01ec638f64445c1d90d2a8df4e1bbad5af7
Angelo Oviedo <angelooviedomont22@gmail.com>
2023-03-30 16:46:16 -0600

    Updates, almost done

--- landing_page_team/assets/js/app.js
+++ landing_page_team/assets/js/app.js
@@ -7,21 +7,3 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
-
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("nav");
-const navLink = document.querySelectorAll("#nav-link");
-
-function toggleMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
