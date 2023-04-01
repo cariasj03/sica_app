f683fd326097afb92b6f82ac7dd755651496f393
cariasj03 <128652671+cariasj03@users.noreply.github.com>
2023-03-31 19:32:42 -0600

    Added details to asset pages

--- js/transfer_forms_provisional_content.js
+++ js/transfer_forms_provisional_content.js
@@ -1,18 +1,16 @@
-window.onload = function(){
+window.onload = function () {
+  let assetId = document.getElementById("assetId");
+  assetId.defaultValue = "000001";
 
-    let assetId = document.getElementById("assetId");
-    assetId.defaultValue = "000001";
+  let assetUnit = document.getElementById("assetOriginUnit");
+  assetUnit.defaultValue = "Heredia";
 
-    let assetUnit = document.getElementById("assetUnit");
-    assetUnit.defaultValue = "Heredia";
+  let assetName = document.getElementById("assetName");
+  assetName.defaultValue = "Impresora EPSON F570";
 
-    let assetName = document.getElementById("assetName");
-    assetName.defaultValue = "Impresora EPSON F570";
+  assetId.disabled = true;
 
-    assetId.disabled = true;
+  assetUnit.disabled = true;
 
-    assetUnit.disabled = true;
-
-    assetName.disabled = true;
-
-}
+  assetName.disabled = true;
+};
